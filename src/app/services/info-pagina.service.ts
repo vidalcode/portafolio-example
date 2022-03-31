d113b7d9018cc8da9ecc1578fd9cfb4776d7e7d6
= <=>
2022-03-30 21:26:16 -0600

    fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(public http: HttpClient) {
     console.log('Servicio de infoPagina listo');
-
+    this.cargarEquipo();
     //leer el archivo JSON
 
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
       console.log( resp.NombreCorto );
       this.cargada = true;
       this.info = resp;
+    })
+  }
+
+  private cargarInfo() {
+    //leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
+      console.log( resp.NombreCorto );
+      this.cargada = true;
+      this.info = resp;
+    })
+  }
+
+  private cargarEquipo(){
+    //leer el archivo JSON
+    this.http.get('https://angular-html-d937b-default-rtdb.firebaseio.com/equipo.json').subscribe( (resp: any ) => {
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(resp);
     })
   }
 }
